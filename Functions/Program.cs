a8f18b2cd603620b25bf015f6d6acdfe4dfcce3a
Janela Pal <janelapalmiste@gmail.com>
2022-10-20 13:37:25 +0300

    add week 7

---
+++ Functions/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Are you coming or leaving? in/out");
+string userChoice = Console.ReadLine();
+if (userChoice== "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+static void PrintHello() //funktsioon
+{
+Console.WriteLine("Hello,world!");
+}
+static void PrintGoodBye()
+{
+    Console.WriteLine($"See you later, aligator.");
+}
